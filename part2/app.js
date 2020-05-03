6589be4c11bcc96c135695318a262d33be9ffa1a
unknown <exgin3538@yahoo.com>
2020-05-03 18:49:18 -0400

    updating app functions

--- part2/app.js
+++ part2/app.js
@@ -1,1 +1,38 @@
-const cardRes = document.querySelector('.cards');
+const $cardRes = $('.cards');
+const $draw = $('.btn');
+
+const deckNum = '3xrla41ed388';
+const baseUrl = 'https://deckofcardsapi.com/api/deck/';
+
+function app() {
+  $draw.on('click', async function () {
+    axios
+      .get(`${baseUrl}/new/shuffle/?deck_count=1`)
+      .then(() => {
+        deck = axios.get(`${baseUrl}/${deckNum}/draw/?count=1`);
+        return deck;
+      })
+      .then((res) => {
+        deck = axios.get(`${baseUrl}/new/draw/?count=1`);
+        return deck;
+      })
+      .then((res) => {
+        for (cards of res.data.cards) {
+          let cardImg = cards.images['png'];
+          let angle = Math.random() * 90 - 45;
+          let randomX = Math.random() * 40 - 20;
+          let randomY = Math.random() * 40 - 20;
+          $cardRes.append(
+            $('<img>', {
+              src: cardImg,
+              css: {
+                transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`,
+              },
+            })
+          );
+        }
+      });
+  });
+}
+
+app();
